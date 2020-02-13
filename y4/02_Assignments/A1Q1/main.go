e48ca945cab751934987a6853c15bb7e8a631968
Radeonxz <xuanxxzhao@gmail.com>
2020-02-12 21:07:19 -0500

    fix: product and sum string

--- y4/02_Assignments/A1Q1/main.go
+++ y4/02_Assignments/A1Q1/main.go
@@ -8,7 +8,6 @@ import (
 func main() {
 	var numerator1, denominator1, numerator2, denominator2 int
 	var fraction1, fraction2, product, sum float32
-	var productStr, sumStr string
 	// ask user to input numerator and denominator for first fraction
 	fmt.Println("Enter numerators and non-zero denominators")
 	fmt.Println("  * for the 1st fraction:")
@@ -20,12 +19,13 @@ func main() {
 	fmt.Scanln(&denominator2)
 
 	// calculate the first fraction and save as float
-	fraction1 = float32(numerator1 / denominator1)
+	fraction1 = float32(numerator1) / float32(denominator1)
 	// concatenate the first fraction and save as string
 	fractionStr1 := strconv.Itoa(numerator1) + "/" + strconv.Itoa(denominator1)
 
 	// calculate the second fraction and save as float
-	fraction2 = float32(numerator2 / denominator2)
+	fraction2 = float32(numerator2) / float32(denominator2)
+	fmt.Println("fraction2 is", fraction2)
 	// concatenate the second fraction and save as string
 	fractionStr2 := strconv.Itoa(numerator2) + "/" + strconv.Itoa(denominator2)
 
@@ -33,6 +33,11 @@ func main() {
 	product = fraction1 * fraction2
 	// calculate the sum of 2 fractions and save as double
 	sum = fraction1 + fraction2
+
+	// concatenate the product
+	productStr := strconv.Itoa(numerator1*numerator2) + "/" + strconv.Itoa(denominator1*denominator2)
+	// concatenate the sum
+	sumStr := strconv.Itoa((numerator1*denominator2)+(numerator2*denominator1)) + "/" + strconv.Itoa(denominator1*denominator2)
 
 	fmt.Println("The product of", fractionStr1, "and", fractionStr2, "is", productStr, "or", product)
 	fmt.Println("The sum of", fractionStr1, "and", fractionStr2, "is", sumStr, "or", sum)
